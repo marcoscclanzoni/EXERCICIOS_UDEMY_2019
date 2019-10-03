9e4f411c4862dcb016691fbe201c6f18989826a9
Marcos Lanzoni <marcoslanzoni@gmail.com>
2019-10-03 08:47:06 -0300

    1 commit

---
+++ hashset.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        HashSet<int> A = new HashSet<int>();
+        HashSet<int> B = new HashSet<int>();
+        A.Add(3);
+        A.Add(5);
+        A.Add(8);
+        A.Add(9);
+        B.Add(3);
+        B.Add(5);
+        B.Add(9);
+        foreach (int x in A)
+        {
+            Console.WriteLine(x);
+        }
+        Console.WriteLine("Digite um numero inteiro ....");
+        int N = int.Parse(Console.ReadLine());
+        if (B.Contains(N))
+        {
+            Console.WriteLine(N + "Pertence ao conjunto b");
+        }
+        else { 
+            Console.WriteLine(N + " Não pertence ao conjunto b");
+        }
+        Console.ReadLine();
+        }
+    }
+
